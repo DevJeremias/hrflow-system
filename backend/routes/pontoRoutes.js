e4ef4086b13077e84cb3f2385f72d00aa769496a
daniel calliari <dbcalliari@gmail.com>
2026-09-13 17:51:51 -0300

    fix(auth): isolate employee time records

--- backend/routes/pontoRoutes.js
+++ backend/routes/pontoRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const pontoController = require('../controllers/pontoController');
+const verificarPerfil = require('../middlewares/roleMiddleware');
+const verificarAcessoFuncionario = require('../middlewares/employeeAccessMiddleware');
 
 router.post('/registrar', pontoController.registrarPonto);
-router.get('/hoje/:funcionarioId', pontoController.listarPontosHoje);
-router.get('/historico/:funcionarioId', pontoController.listarHistorico);
-router.get('/totais/:funcionarioId', pontoController.listarTotais);
-router.get('/', pontoController.listarPontos);
+router.get('/hoje/:funcionarioId', verificarAcessoFuncionario, pontoController.listarPontosHoje);
+router.get('/historico/:funcionarioId', verificarAcessoFuncionario, pontoController.listarHistorico);
+router.get('/totais/:funcionarioId', verificarAcessoFuncionario, pontoController.listarTotais);
+router.get('/', verificarPerfil(['Administrador', 'RH']), pontoController.listarPontos);
 
 module.exports = router;
